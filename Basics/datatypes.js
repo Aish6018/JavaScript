a8cbb0eac71662c8a335bf7316e8cbc38fb7c2dd
Aish6018 <102402817+Aish6018@users.noreply.github.com>
2024-04-10 07:38:39 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"  //treats all JS code as newer version
+
+//alert(3+3)  //it won't work as we are using nodejs not browser
+
+console.log("Aishwarya")
+
+let name = "Aish"
+let age = 22
+let isLoggedIn = true
+
+
+console.log(typeof "Aish")
+console.log(typeof age)
+
+
+//typeof null is object 
+//typeof undefined is undefined
