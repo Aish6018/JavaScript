e3d221ef382efe5d6105afda2bd24267e5420a76
Aish6018 <102402817+Aish6018@users.noreply.github.com>
2024-04-10 11:02:50 +0000

    conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,23 @@
+let score = "33";
+console.log(typeof(score));
+
+
+let value = Number(score);
+console.log(typeof(value));
+
+//"33" = 33
+//"33ab" = NaN
+//true = 1; false = 0;
+
+let someval = 0;
+let booleanval = Boolean(someval);
+console.log(booleanval);
+//1 = ture; 0 = false;
+//"" = false
+//"Aish" = true
+
+
+let number = 33;
+let stringno = String(number);
+console.log(stringno);
+console.log(typeof stringno);
