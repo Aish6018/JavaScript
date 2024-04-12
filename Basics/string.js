ce4b8f0d15b5fee98caa414bd167b70d9f76e3ca
Aish6018 <102402817+Aish6018@users.noreply.github.com>
2024-04-12 04:35:26 +0000

    string and num

---
+++ Basics/string.js
@@ -0,0 +1,18 @@
+const name = "Aishwarya"
+const lname = "Chavan"
+const rn = 7
+console.log(name + rn)
+
+console.log(`Hello my name is ${name} and my last name is ${lname}`)
+
+
+const gameName = new String("Aish-ac-com")
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+console.log(gameName.replace("-","."))
+const newstr = gameName.slice(0, 4)
+console.log(newstr)
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf("i"))
+console.log(gameName.split("-"))
