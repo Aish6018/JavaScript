50ab64cbd71e38f61c748f8601a30643031f2b5e
Aish6018 <102402817+Aish6018@users.noreply.github.com>
2024-04-13 07:24:56 +0000

    array

--- Basics2/array2.js
+++ Basics2/array2.js
@@ -1,1 +1,20 @@
 const marvel = ["Thor", "IronMan","SpiderMan"]
+
+const dc = ["Superman", "Batman", "Robin"]
+
+// marvel.push(dc);   //array inside and array
+// console.log(marvel);
+
+
+const all = marvel.concat(dc)
+console.log(all);
+
+const difall = [...marvel, ...dc]
+console.log(difall);
+
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+
+console.log(Array.of(score1, score2, score3))
